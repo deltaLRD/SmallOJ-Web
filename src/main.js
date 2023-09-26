c59f21b88ab3606c6359e6bc6f7f49bf52a74763
deltaLRD <ruidongli2002@gmail.com>
2023-09-26 19:45:52 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue' 
+import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './components/router/router.js'
@@ -9,13 +9,14 @@ import 'element-plus/dist/index.css'
 import VueCookies from 'vue3-cookies'
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:4523/m1/3227171-0-default/',
-  });
+  baseURL: 'http://127.0.0.1:8080',
+});
 
 
 
 const app = createApp(App)
-
+app.use(VueCookies)
+// app.config.globalProperties.$cookies = VueCookies
 app.config.globalProperties.$api = api;
 app.use(ElementPlus)
 app.use(router)
